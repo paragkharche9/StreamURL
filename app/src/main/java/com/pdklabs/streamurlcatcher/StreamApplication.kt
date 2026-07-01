794b8792e67c021aa4a307439f313538d0023335
Parag Kharche <parag12067@gmail.com>
2026-07-02 00:02:14 +0530

    Enhance logging in StreamApplication and update backup rules; streamline data extraction configuration

--- app/src/main/java/com/pdklabs/streamurlcatcher/StreamApplication.kt
+++ app/src/main/java/com/pdklabs/streamurlcatcher/StreamApplication.kt
@@ -1,6 +1,7 @@
 package com.pdklabs.streamurlcatcher
 
 import android.app.Application
+import android.util.Log
 import com.yausername.youtubedl_android.YoutubeDL
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -8,6 +9,8 @@ import kotlinx.coroutines.launch
 
 class StreamApplication : Application() {
     override fun onCreate() {
+        val tag = "StreamApplication"
+
         super.onCreate()
         try {
             YoutubeDL.getInstance().init(this)
@@ -16,9 +19,13 @@ class StreamApplication : Application() {
             // The library automatically checks if the local version is outdated before downloading
             CoroutineScope(Dispatchers.IO).launch {
                 try {
+                    Log.d(tag, "Updating youtube-dl...")
                     YoutubeDL.getInstance().updateYoutubeDL(this@StreamApplication, YoutubeDL.UpdateChannel.STABLE)
-                } catch (_: Exception) {}
+                    Log.d(tag, "youtube-dl update check complete.")
+                } catch (e: Exception) {
+                    Log.e(tag, "Failed to update youtube-dl", e)
+                }
             }
-        } catch (_: Exception) {}
+        } catch (e: Exception) { Log.e(tag, "Failed to initialize YoutubeDL", e) }
     }
 }
